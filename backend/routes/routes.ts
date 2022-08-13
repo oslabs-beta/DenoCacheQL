adb9fa298a2a7c86a49844fe0e70fe8160a6afb2
MHindt <75388368+MHindt@users.noreply.github.com>
2022-08-13 13:22:02 -0400

    Deno server running on port 3000

--- backend/routes/routes.ts
+++ backend/routes/routes.ts
@@ -3,6 +3,11 @@ import { Router } from 'https://deno.land/x/oak/mod.ts';
 //create an instance of router
 const router = new Router();
 
+router.get('/', ({ response }: { response: any }) => {
+    response.body = {
+        message: 'hello world',
+    };
+});
 
 
 
--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,20 @@
 //using Oak a middleware framework for Deno
 import { Application } from 'https://deno.land/x/oak/mod.ts';
+
 //import Router
 import router from './routes/routes.ts';
 //create an instance of application
 const app = new Application();
-const PORT = 3000;
+const port = 3000;
 //Implementing router
 app.use(router.routes());
 //allow router HTTP methods
 app.use(router.allowedMethods());
-console.log(`Server listening on Port: ${PORT}`);
-await app.listen( {port: PORT});
+
+app.addEventListener('listen', ({ secure, hostname, port}) => {
+    const protocol = secure ? 'https://' : 'http://';
+    const url = `${protocol}${hostname ?? "localhost"}: ${port}`;
+    console.log(`Listening on: ${port}`);
+})
+console.log("hello world");
+await app.listen( { port });
