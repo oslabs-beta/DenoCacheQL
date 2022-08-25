12b92d3d760775e107d32808557b84270562f26c
Jessica Balding <jballey@harding.edu>
2022-08-24 21:14:37 -0600

    Working on stringify database result for redis

--- backend/graphql.ts
+++ backend/graphql.ts
@@ -1,6 +1,7 @@
 
 import { client } from './server.ts'
 import { gql } from 'https://deno.land/x/oak_graphql/mod.ts';
+import { redis } from './redis.ts'
 
 const typeDefs = gql`
   type People {
@@ -24,8 +25,23 @@ const typeDefs = gql`
   const resolvers = {
     Query: {
         getPeople: async() => {
-            const person = await client.queryObject('SELECT * FROM people WHERE _id=1');
-            return person.rows;
+            const redisKey = 'SELECT name FROM people WHERE _id=3'
+            //const person = await client.queryObject('SELECT * FROM people WHERE _id=1');
+            //look in the cache for the provided query
+            const person = await redis.exists(redisKey) 
+            if (!person) {
+              console.log("entered conditional")
+               //otherwise, we make the call to the client
+               const character = await client.queryObject<string>(redisKey)
+               character.toString()
+               //console.log("Character.rows", character.rows)
+               //then save the query and the response as the key value pair in redis
+               await redis.set(redisKey, character.toString())
+                    // //return the responsect
+              return character.rows;
+            }
+            //if we find the value, then return
+            return await redis.get(redisKey)
         }
     }
   }
