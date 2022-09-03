0ac18a49e9e8d1a0b66ef35f188ae8fb7e9ac16b
EdnaK18 <reginakwanrx@gmail.com>
2022-09-03 12:55:30 -0400

    add variables in query

--- server/graphql.ts
+++ server/graphql.ts
@@ -15,46 +15,45 @@ const typeDefs = gql`
     height: Int
   }
   type Query {
-    getPeople (characterNumber: Int): [People]
+    getPeople(characterNumber: Int): [People]
   }
 `;
 
 const resolvers = {
   Query: {
     getPeople: async (parent: any, arg: any, context: any, info: any) => {
-      console.log("arg", arg)
+      // console.log('arg', arg);
       
-      const redisKey = `SELECT name FROM people WHERE _id=${arg.characterNumber}`;
+      const redisKey = `SELECT name, gender, mass, hair_color, skin_color, eye_color,birth_year, height FROM people WHERE _id=${arg.characterNumber}`;
       //const person = await client.queryObject('SELECT * FROM people WHERE _id=1');
       //look in the cache for the provided query
       console.time();
       const person = await redis.exists(redisKey);
       if (!person) {
-        console.log('entered conditional');
+        console.log('entered conditional--- getting from sql server');
         //otherwise, we make the call to the client
         const character = await client.queryObject<string>(redisKey);
         console.log('Character.rows', character.rows);
         //then save the query and the response as the key value pair in redis
         await redis.set(redisKey, JSON.stringify(character.rows));
         // //return the responsect
-        const timeFromServer = console.timeEnd();
+        console.timeEnd();
         return character.rows;
       }
       //if we find the value, then return
-      const formatThis = await redis.get(redisKey);
-     
-      console.log('format....', formatThis);
-      if(typeof formatThis !== 'string'){
-        
-      let format = JSON.stringify(formatThis);
-      return JSON.parse(format)
 
-      } else{
-      let formattedResponse = JSON.parse(formatThis);
-      return formattedResponse
+//---------- return result and latency from redis ------------
+      const formatThis = await redis.get(redisKey);
+      console.timeEnd();
+      console.log('format....getting from redis', formatThis);
+      if (typeof formatThis !== 'string') {
+        let format = JSON.stringify(formatThis);
+        return JSON.parse(format);
+      } else {
+        let formattedResponse = JSON.parse(formatThis);
+        return formattedResponse;
       }
-     
-      
+
       // console.log('after formatting....', formatThis);
       // const timeFromCache = console.timeEnd();
       // return format;
