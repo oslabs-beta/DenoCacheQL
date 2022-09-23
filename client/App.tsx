a8b06563f63400444e390cee8b71bebfe5624fb8
EdnaK18 <reginakwanrx@gmail.com>
2022-09-22 23:59:58 -0400

    fix rendering results on submit

--- client/App.tsx
+++ client/App.tsx
@@ -1,30 +1,35 @@
 //import react
-import React, {
-  useState,
-  useEffect,
-  useRef,
-} from 'https://esm.sh/react@18.2.0';
+import React from 'https://esm.sh/react@18.2.0';
 import ReactDOM from 'https://esm.sh/react-dom@18.2.0';
 import RequestForm from './requestForm.tsx';
 
 //main app container
 
 const App = () => {
-  const [loadedResults, setLoadedResults] = React.useState(false);
+  const [loadedResults, setLoadedResults] = React.useState(true);
   const [queryInputNumber, setQueryInputNumber] = React.useState<
     string | undefined
   >(undefined);
   // const [queryResponse, setQueryResponse] = React.useState('');
   // const [responseTime, setResponseTime] = React.useState('');
   const [queryHistory, setQueryHistory] = React.useState([]);
+  // let queryHistoryElements = '';
 
-  React.useEffect(() => {
+  // React.useEffect(() => {
+  //   console.log('in useEffect');
+  //   queryHistory.map((name: any, i: number) => {
+  //     queryHistoryElements.concat('<li>' + name + '</li>');
+  //   });
+  //   setLoadedResults(false);
+  // }, queryHistory);
+
+  const handleSubmitQuery = async (e) => {
+    e.preventDefault();
     setLoadedResults(true);
-  }, queryHistory);
-
-  const handleSubmitQuery = async () => {
-    console.log(queryInputNumber);
-    const inputNumber: string | undefined = queryInputNumber;
+    console.dir(e);
+    console.log(e.target[0].value);
+    // console.log(queryInputNumber);
+    const inputNumber: string | undefined = e.target[0].value;
     let queryResponse: string;
     // const query: string = `query getPeople($queryNumber: Int){getPeople()}`;
     try {
@@ -54,10 +59,11 @@ const App = () => {
       // await setQueryHistory(...queryHistory, queryResponse);
       queryHistory.push(queryResponse);
       console.log('queryHistory', queryHistory);
-      setLoadedResults(false);
     } catch (error) {
       console.log('error--->', error);
     }
+    setLoadedResults(false);
+    // setQueryHistory(...queryHistory, queryResponse);
   };
 
   return (
@@ -65,30 +71,31 @@ const App = () => {
       <div className="app">
         <h1>rendering app.tsx</h1>
         <div className="requestForm">
-          <input
-            type="text"
-            onChange={(e) => {
-              console.log('e --->', e);
-              setQueryInputNumber(e.target.value);
+          <form
+            onSubmit={(e) => {
+              handleSubmitQuery(e);
             }}
-          />
-          <button type="button" onClick={handleSubmitQuery}>
-            Click Me
-          </button>
+          >
+            <input
+              id="inputNumber"
+              type="text"
+              // onChange={(e) => {
+              //   console.log('e --->', e);
+              //   setQueryInputNumber(e.target.value);
+              // }}
+            />
+            <button type="submit">Click Me</button>
+          </form>
         </div>
       </div>
       <div className="results">
         <h1>Results container</h1>
-        {/* {queryHistory} */}
-    
+
         <ul>
           {queryHistory.map((name: any, i: number) => {
-          return(
-            <li >{name}</li>
-          )
+            return <li>{name}</li>;
           })}
         </ul>
-  
       </div>
     </>
   );
